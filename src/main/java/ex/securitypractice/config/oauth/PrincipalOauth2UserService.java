872d5b278cae82d4e3cc29ece0c41efd8c5aed46
Seong Heung Bae <mopil1102@gmail.com>
2022-08-31 22:09:33 +0900

    네이버 로그인 구현

--- src/main/java/ex/securitypractice/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/ex/securitypractice/config/oauth/PrincipalOauth2UserService.java
@@ -3,6 +3,7 @@ package ex.securitypractice.config.oauth;
 import ex.securitypractice.config.auth.PrincipalDetails;
 import ex.securitypractice.config.oauth.provider.FacebookUserInfo;
 import ex.securitypractice.config.oauth.provider.GoogleUserInfo;
+import ex.securitypractice.config.oauth.provider.NaverUserInfo;
 import ex.securitypractice.config.oauth.provider.OAuth2UserInfo;
 import ex.securitypractice.model.User;
 import ex.securitypractice.repository.UserRepository;
@@ -13,6 +14,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -35,17 +38,24 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
         String providerName = userRequest.getClientRegistration().getRegistrationId(); // google
         OAuth2UserInfo oAuth2UserInfo = null;
-        if (providerName.equals("google")) {
-            System.out.println("구글 로그인 요청");
-            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
-        } else if (providerName.equals("facebook")) {
-            System.out.println("페이스북 로그인 요청");
-            oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
-        } else {
-            System.out.println("우리는 구글과 페이스북만 지원해요");
+        switch (providerName) {
+            case "google" -> {
+                System.out.println("구글 로그인 요청");
+                oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+            }
+            case "facebook" -> {
+                System.out.println("페이스북 로그인 요청");
+                oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+            }
+            case "naver" -> {
+                System.out.println("네이버 로그인 요청");
+                oAuth2UserInfo = new NaverUserInfo((Map<String, Object>) oAuth2User.getAttributes().get("response"));
+            }
+            default -> System.out.println("우리는 구글과 페이스북, 네이버만 지원해요");
         }
 
         // OAuth2 로 받아온 정보로 강제 회원가입 진행
+        assert oAuth2UserInfo != null;
         String provider = oAuth2UserInfo.getProvider();
         String providerId = oAuth2UserInfo.getProviderId();
         String username = provider+"_"+providerId; // google_5234523412341235 (유저네임 유니크화)
